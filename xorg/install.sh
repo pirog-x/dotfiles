e4fe03e0710f308f18282593d8248f15810f70ba
Vitalik <vitalikvlasenko88@gmail.com>
2023-10-29 14:06:53 +0100

    Add xorg, kitty, bspwm and sxhkd configs

---
+++ xorg/install.sh
@@ -0,0 +1,18 @@
+# This is dependencies from video
+# sudo dnf -y install \
+# x11-libs/libX11 \
+# x11-base/xorg-server \
+# x11-libs/libXrandr \
+# x11-libs/libXinerama \
+# x11-libs/libXft \
+# x11-apps/xinit \
+# x11-apps/xrdb \
+# x11-apps/mesa-progs \
+# x11-apps/xrandr \
+# x11-drivers/xf86-video-vboxvideo \
+# x11-misc/unclutter \
+# x11-misc/xclip \
+# x11-misc/pcmanfm
+
+# This is how to reload file in system
+# $ xrdb /home/pirog/.Xresources
